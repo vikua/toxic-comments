4cc2897975151b03326e8dbb8348b46a83b5cea0
Viktor Kovryzhkin <vik.kovrizhkin@gmail.com>
2018-08-19 15:12:11 +0300

    vocabulary processor speed up and other changes

--- model.py
+++ model.py
@@ -1,8 +1,20 @@
+import multiprocessing as mp
+from functools import partial
+
 import numpy as np
 import tensorflow as tf 
 
 from nltk import word_tokenize
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+def chunks(lst, chunk_size): 
+    for i in range(0, len(lst), chunk_size): 
+        yield lst[i:i + chunk_size]
+
+
+def tokenize_list(lst): 
+    return [word_tokenize(s.lower()) for s in lst]
 
 
 class VocabularyProcessor(object):
@@ -24,17 +36,35 @@ class VocabularyProcessor(object):
     def word_to_index(self):
         return self._word_to_index
 
-    def fit_transform(self, data): 
-        self._max_seq_len = max([len(sentence) for sentence in data])
+    def _execute_parallel(self, data, func): 
+        pool = mp.Pool(mp.cpu_count())
+        pool_results = pool.map(func, list(chunks(data, 1000)))
+        pool.close()
+        pool.join()
 
-        sentences = [word_tokenize(s) for s in data]
+        results = []
+        for result in pool_results:
+            results.extend(result)
+
+        return results
+
+    def fit_transform(self, data): 
+        print('Building vocabulary...')
+        sentences = self._execute_parallel(data, tokenize_list)
+        self._max_seq_len = max([len(sentence) for sentence in sentences])
+
         words = set([word for sentence in sentences for word in sentence])
 
         self._vocab = ['<unk>'] + sorted(words)
         self._word_to_index = {word: i + 1 for i, word in enumerate(self._vocab)}
 
-        sentences = [self._vectorize_sentence(s) for s in sentences]
+        print('Mapping words to indices...')
+        sentences = self._execute_parallel(sentences, self._vectorize)
+
+        print('Padding sequences...')
         sentences = pad_sequences(sentences, maxlen=self._max_seq_len, value=0, padding='post')
+        
+        print('Done!')
         return sentences
 
     def transform(self, data): 
@@ -49,13 +79,12 @@ class VocabularyProcessor(object):
 
         return sentences
 
+    def _vectorize(self, sentences): 
+        return [self._vectorize_sentence(sentence) for sentence in sentences]
+
     def _vectorize_sentence(self, sentence): 
-        array = np.empty_like(sentence, dtype=np.int32)
-        for i, word in enumerate(sentence): 
-            if word not in self._vocab: 
-                word = '<unk>'
-            array[i] = self._word_to_index[word]
-        return array
+        unk = self._word_to_index['<unk>']
+        return np.array([self._word_to_index.get(word, unk) for word in sentence])
 
 
 class NNClassifier(object):
@@ -73,8 +102,9 @@ class NNClassifier(object):
 
     def build_model(self): 
         inputs = tf.keras.Input(shape=(self._vocab_processor.max_seq_len, ), dtype='int32', name='inputs')
-        embed = tf.keras.layers.Embedding(len(self._vocab_processor.vocab), 
+        embed = tf.keras.layers.Embedding(len(self._vocab_processor.vocab) + 1, 
                                           self._embedding_dim,
+                                          mask_zero=True,
                                           trainable=True)(inputs)
         dropout = tf.keras.layers.Dropout(self._dropout)(embed)
         lstm = tf.keras.layers.LSTM(self._lstm_units)(dropout)
--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import os
 import argparse
-import pickle
 
 import pandas as pd
 import numpy as np
@@ -12,35 +11,13 @@ TEXT_COL = 'comment_text'
 CLASSES = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
 
-def prepare_data(args): 
-    x_path = os.path.join(args.output_dir, 'X.npy')
-    y_path = os.path.join(args.output_dir, 'y.npy')
-    vocab_path = os.path.join(args.output_dir, 'vocab.pkl')
-    if os.path.exists(x_path): 
-        os.remove(x_path)
-    if os.path.exists(y_path): 
-        os.remove(y_path)
-    if os.path.exists(vocab_path): 
-        os.remove(vocab_path)
-
-    data = pd.read_csv(args.data_path)
-
-    vocab_processor = VocabularyProcessor()
-    X = vocab_processor.fit_transform(data[TEXT_COL].values)
-    y = data[CLASSES].values
-
-    with open(vocab_path, 'wb') as f: 
-        pickle.dump(vocab_processor, f)
-    np.save(x_path, X)
-    np.save(y_path, y)
-
-
 def train(args): 
-    X = np.load(os.path.join(args.input_path, 'X.npy'))
-    y = np.load(os.path.join(args.input_path, 'y.npy'))
+    data = pd.read_csv(os.path.join(args.input_path, 'train.csv'))
 
-    with open(os.path.join(args.input_path, 'vocab.pkl'), 'rb') as f: 
-        vocab_processor = pickle.load(f)
+    vp = VocabularyProcessor()
+    texts = vp.fit_transform([TEXT_COL].values)
+
+    labels = data[CLASSES].values
 
     clf = NNClassifier(len(CLASSES), vocab_processor,
                        embedding_dim=args.embedding_dim, 
@@ -52,27 +29,16 @@ def train(args):
 
 if __name__ == '__main__': 
     parser = argparse.ArgumentParser(description='Simple toxic comments classifier')
-    subs = parser.add_subparsers(title='command', dest='command')
 
-    vocab_parser = subs.add_parser('prepare_data')
-    vocab_parser.add_argument('--data-path', dest='data_path', type=str, 
-                              help='Path to raw train data')
-    vocab_parser.add_argument('--output-dir', dest='output_dir', type=str, 
-                              help='Path to output directory')
-
-    train_parser = subs.add_parser('train')
-    train_parser.add_argument('--input-path', dest='input_path', type=str, 
-                              help='Path to input ')
-    train_parser.add_argument('--epochs', dest='epochs', type=int, default=5,
-                              help='Number of epochs')
-    train_parser.add_argument('--batch-size', dest='batch_size', type=int, default=64,
-                              help='Batch size')
-    train_parser.add_argument('--dropout-keep-prob', dest='dropout_keep_prob', type=float, 
+    parser.add_argument('--input-path', dest='input_path', type=str, 
+                        help='Path to input ')
+    parser.add_argument('--epochs', dest='epochs', type=int, default=5,
+                        help='Number of epochs')
+    parser.add_argument('--batch-size', dest='batch_size', type=int, default=64,
+                        help='Batch size')
+    parser.add_argument('--dropout-keep-prob', dest='dropout_keep_prob', type=float, 
                         default=0.5, help='Dropout keep probability')
-    train_parser.add_argument('--embedding-dim', dest='embedding_dim', type=int, default=100)
+    parser.add_argument('--embedding-dim', dest='embedding_dim', type=int, default=100)
 
     args = parser.parse_args()
-    if args.command == 'prepare_data': 
-        prepare_data(args)
-    elif args.command == 'train': 
-        train(args)
+    # train(args)
